04fe8ad68efb1e1ef7e02ea2ad0c2eb3756f899c
Rizwan <rizwanmelepath@gmail.com>
2024-12-30 13:27:50 +0530

    Here i have completed the TMDB movies listing

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,24 @@
 import React, { useEffect, useRef } from 'react'
 import './TitleCards.css'
 import cards_data  from '../../assets/cards/Cards_data'
-
+import { useState } from 'react'
 const TitleCards = ({title,category}) => {
 
+const [apiData, setApiData] = useState([]);
 const cardsRef = useRef();
+
+
+
+
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Mzg4YjUxN2RjYWY3Y2ZkOGNiMDdmOWQyZDdjZDQzNiIsIm5iZiI6MTczNTU0MTYyMS40NDUsInN1YiI6IjY3NzI0Mzc1NjNmOTBmOGY2NjkyNzA4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cj7I5Hi98ezD3QDG99RTZMwa7y7aWeaa4G4SSypLe1Y'
+    }
+  };
+  
+  
 
 const handleWheel = (event)=>{
     event.preventDefault();
@@ -12,6 +26,12 @@ const handleWheel = (event)=>{
 }
 
 useEffect(()=>{
+
+    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results))
+    .catch(err => console.error(err));
+
     cardsRef.current.addEventListener('Wheel', handleWheel);
 })
 
@@ -23,10 +43,10 @@ useEffect(()=>{
         <h2>{title?title:"Popular on Netflix"}</h2>
         
         <div className="card-list" ref={cardsRef}>
-            {cards_data.map((card,index)=>{
+            {apiData.map((card,index)=>{
                 return <div className="card" key={index}>
-                    <img src={card.image} alt="" />
-                    <p>{card.name}</p>
+                    <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+                    <p>{card.original_title}</p>
                 </div>
             })}
         </div>
