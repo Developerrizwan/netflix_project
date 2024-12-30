8bd2047165fe49007e5dcde0a4c62ce5e03f5522
Rizwan <rizwanmelepath@gmail.com>
2024-12-30 12:15:36 +0530

    Here i have completed the main page session and login session

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import Home from './pages/Home/Home'
+import './index.css'
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login/Login'
 const App = () => {
   return (
     <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
       
     </div>
   )
